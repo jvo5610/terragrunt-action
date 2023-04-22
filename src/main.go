7ae0eb603ef4b57365a8169996c8251b42fe728f
José Videla Olmos <jvidelaolmos@gmail.com>
2023-04-22 17:12:14 -0300

    update

--- src/main.go
+++ src/main.go
@@ -2,49 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
-	"strings"
 )
 
 func main() {
-	wd, err := os.Getwd()
+	fmt.Println("GitHub repository owner:", os.Getenv("GITHUB_REPOSITORY_OWNER"))
+	fmt.Println("GitHub repository name:", os.Getenv("GITHUB_REPOSITORY_NAME"))
+	fmt.Println("GitHub event name:", os.Getenv("GITHUB_EVENT_NAME"))
+	fmt.Println("GitHub ref:", os.Getenv("GITHUB_REF"))
+	fmt.Println("GitHub sha:", os.Getenv("GITHUB_SHA"))
+	fmt.Println("GitHub event path:", os.Getenv("GITHUB_EVENT_PATH"))
 
-	// Check if a Terragrunt command is provided as a command-line argument
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: ./terragrunt <terragrunt_command>, <terragrunt_path>")
-	}
-
-	arguments := strings.Join(os.Args[1:], " ")
-	args := strings.Split(arguments, ",")
-
-	terragruntCommand := args[0]
-	terragruntPath := func(args []string) string {
-		if len(args) > 1 {
-			return strings.TrimSpace(args[1])
-		}
-		return ""
-	}(args)
-
-	// Set up the Terragrunt command with the provided command-line argument
-	cmd := exec.Command("terragrunt", terragruntCommand)
-
-	// Set the working directory if needed
-	cmd.Dir = wd + terragruntPath
-
-	// Set environment variables if needed
-	//cmd.Env = append(os.Environ(), "AWS_ACCESS_KEY_ID=your_aws_access_key", "AWS_SECRET_ACCESS_KEY=your_aws_secret_access_key")
-
-	// Redirect the command's stdout and stderr to os.Stdout and os.Stderr respectively
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	// Run the Terragrunt command
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Terragrunt command completed successfully!")
+	// Access other environment variables for more context information as needed
 }
