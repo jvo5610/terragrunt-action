6b7f052645ea0ec42a6107235c9e317c315e11b5
José Videla Olmos <jvidelaolmos@gmail.com>
2023-04-22 17:14:52 -0300

    update

--- src/main.go
+++ src/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	fmt.Println("GitHub repository owner:", os.Getenv("GITHUB_REPOSITORY_OWNER"))
-	fmt.Println("GitHub repository name:", os.Getenv("GITHUB_REPOSITORY_NAME"))
-	fmt.Println("GitHub event name:", os.Getenv("GITHUB_EVENT_NAME"))
-	fmt.Println("GitHub ref:", os.Getenv("GITHUB_REF"))
-	fmt.Println("GitHub sha:", os.Getenv("GITHUB_SHA"))
-	fmt.Println("GitHub event path:", os.Getenv("GITHUB_EVENT_PATH"))
+	eventPath := os.Getenv("GITHUB_EVENT_PATH")
+	if eventPath == "" {
+		fmt.Println("GITHUB_EVENT_PATH environment variable is not set")
+		return
+	}
 
-	// Access other environment variables for more context information as needed
+	eventData, err := ioutil.ReadFile(eventPath)
+	if err != nil {
+		fmt.Println("Failed to read GITHUB_EVENT_PATH:", err)
+		return
+	}
+
+	var event interface{}
+	err = json.Unmarshal(eventData, &event)
+	if err != nil {
+		fmt.Println("Failed to parse GITHUB_EVENT_PATH:", err)
+		return
+	}
+
+	fmt.Println("Contents of GITHUB_EVENT_PATH:")
+	fmt.Println(string(eventData))
 }
